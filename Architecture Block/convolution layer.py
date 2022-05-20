9340c5f7caea3df683a178926233c41cbce16304
Radeen Mostafa <66905164+RadeenXALNW@users.noreply.github.com>
2022-05-21 01:49:59 +0600

    Create convolution layer.py

---
+++ Architecture Block/convolution layer.py
@@ -0,0 +1,23 @@
+class Conv2dReLU(nn.Sequential):
+    def __init__(
+            self,
+            in_channels,
+            out_channels,
+            kernel_size,
+            padding=0,
+            stride=1,
+            use_batchnorm=True,
+    ):
+        conv = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride=stride,
+            padding=padding,
+            bias=not (use_batchnorm),
+        )
+        relu = nn.ReLU(inplace=True)
+
+        bn = nn.BatchNorm2d(out_channels)
+
+        super(Conv2dReLU, self).__init__(conv, bn, relu)
