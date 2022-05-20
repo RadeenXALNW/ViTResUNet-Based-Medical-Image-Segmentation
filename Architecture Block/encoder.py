a89f5d513acb6ae7c666c46d90f697ad0ad81ab5
Radeen Mostafa <66905164+RadeenXALNW@users.noreply.github.com>
2022-05-21 01:48:54 +0600

    encoder for the architecture

---
+++ Architecture Block/encoder.py
@@ -0,0 +1,18 @@
+class Encoder(nn.Module):
+    def __init__(self, config, vis):
+        super(Encoder, self).__init__()
+        self.vis = vis
+        self.layer = nn.ModuleList()
+        self.encoder_norm = LayerNorm(config.hidden_size, eps=1e-6)
+        for _ in range(config.transformer["num_layers"]):
+            layer = Block(config, vis)
+            self.layer.append(copy.deepcopy(layer))
+
+    def forward(self, hidden_states):
+        attn_weights = []
+        for layer_block in self.layer:
+            hidden_states, weights = layer_block(hidden_states)
+            if self.vis:
+                attn_weights.append(weights)
+        encoded = self.encoder_norm(hidden_states)
+        return encoded, attn_weights
