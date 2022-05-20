c01f6a2768da6c4d199021f2f2832b4da65538ed
Radeen Mostafa <66905164+RadeenXALNW@users.noreply.github.com>
2022-05-21 01:51:42 +0600

    Create segmentation head.py

---
+++ Architecture Block/segmentation head.py
@@ -0,0 +1,6 @@
+class SegmentationHead(nn.Sequential):
+
+    def __init__(self, in_channels, out_channels, kernel_size=3, upsampling=1):
+        conv2d = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=kernel_size // 2)
+        upsampling = nn.UpsamplingBilinear2d(scale_factor=upsampling) if upsampling > 1 else nn.Identity()
+        super().__init__(conv2d, upsampling)
