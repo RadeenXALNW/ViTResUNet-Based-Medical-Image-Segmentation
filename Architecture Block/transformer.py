9a8747e48a0f43bb1e7c9db479e924cb7c86b263
Radeen Mostafa <66905164+RadeenXALNW@users.noreply.github.com>
2022-05-21 01:50:17 +0600

    Create transformer.py

---
+++ Architecture Block/transformer.py
@@ -0,0 +1,10 @@
+class Transformer(nn.Module):
+    def __init__(self, config, img_size, vis):
+        super(Transformer, self).__init__()
+        self.embeddings = Embeddings(config, img_size=img_size)
+        self.encoder = Encoder(config, vis)
+
+    def forward(self, input_ids):
+        embedding_output, features = self.embeddings(input_ids)
+        encoded, attn_weights = self.encoder(embedding_output)  # (B, n_patch, hidden)
+        return encoded, attn_weights, features
